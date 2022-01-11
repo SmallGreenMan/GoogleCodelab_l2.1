3f9d805629d6fcc075729d4957c45f2cc46bcdd9
Alexandr Voishel <avoishel@gmail.com>
2022-01-12 00:34:16 +0300

    add: step 2: Create and launch the second Activity

---
+++ app/src/main/java/android/examample/googlecodelab_21_twoactivities/SecondActivity.java
@@ -0,0 +1,14 @@
+package android.examample.googlecodelab_21_twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
