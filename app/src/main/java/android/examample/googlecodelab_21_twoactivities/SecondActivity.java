619894703a3d99eb01d98371e0221071b483b5b3
Alexandr Voishel <avoishel@gmail.com>
2022-01-12 15:46:45 +0300

    add: step 3: Send data from the main Activity to the second Activity

--- app/src/main/java/android/examample/googlecodelab_21_twoactivities/SecondActivity.java
+++ app/src/main/java/android/examample/googlecodelab_21_twoactivities/SecondActivity.java
@@ -2,7 +2,9 @@ package android.examample.googlecodelab_21_twoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String messages = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(messages);
+
     }
 }
